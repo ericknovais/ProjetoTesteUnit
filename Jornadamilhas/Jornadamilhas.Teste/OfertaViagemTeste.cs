9900afc6d5764e34f6b7a2b21ccaa3887e17c213
ericknovais <erick.hora@outlook.com>
2024-04-09 21:16:56 -0300

    padrão AAA

--- Jornadamilhas/Jornadamilhas.Teste/OfertaViagemTeste.cs
+++ Jornadamilhas/Jornadamilhas.Teste/OfertaViagemTeste.cs
@@ -7,13 +7,35 @@ namespace Jornadamilhas.Teste
         [Fact]
         public void TestandoOfertaValida()
         {
-            Rota rota = new Rota("OrigemTeste","DestinoTeste");
+            // cenário -- arrange
+            Rota rota = new Rota("OrigemTeste", "DestinoTeste");
             Periodo periodo = new Periodo(new DateTime(2024, 2, 1), new DateTime(2024, 2, 5));
             double preco = 100.0;
             var validacao = true;
+
+            //ação - act
+            OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
+            
+            //Validação - assert
+            Assert.Equal(validacao, oferta.EhValido);
+
+        }
+
+        [Fact]
+        public void TestandoOfertaComRotaNula()
+        {
+            //Padrão AAA
+            // cenário -- arrange
+            Rota rota = null;
+            Periodo periodo = new Periodo(new DateTime(2024, 2, 1), new DateTime(2024, 2, 5));
+            double preco = 100.0;
+
+            //ação - act
             OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
 
-            Assert.Equal(validacao, oferta.EhValido);
+            //Validação - assert
+            Assert.Contains("A oferta de viagem não possui rota ou período válidos.", oferta.Erros.Sumario);
+            Assert.False(oferta.EhValido);
 
         }
     }
