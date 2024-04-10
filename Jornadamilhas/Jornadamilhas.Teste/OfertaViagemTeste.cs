a7ccfc20554ea73a781e6682dbf0258afdf44d34
ericknovais <erick.hora@outlook.com>
2024-04-09 21:04:39 -0300

    Primeiro teste automatizado

---
+++ Jornadamilhas/Jornadamilhas.Teste/OfertaViagemTeste.cs
@@ -0,0 +1,20 @@
+using JornadaMilhasV1.Modelos;
+
+namespace Jornadamilhas.Teste
+{
+    public class OfertaViagemTeste
+    {
+        [Fact]
+        public void TestandoOfertaValida()
+        {
+            Rota rota = new Rota("OrigemTeste","DestinoTeste");
+            Periodo periodo = new Periodo(new DateTime(2024, 2, 1), new DateTime(2024, 2, 5));
+            double preco = 100.0;
+            var validacao = true;
+            OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
+
+            Assert.Equal(validacao, oferta.EhValido);
+
+        }
+    }
+}
